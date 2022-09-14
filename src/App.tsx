caaa4ca891cc46a06ba8e1d58f55fa8b3f0ebbc0
Rushil-2311 <rushilpatellinux2gmail.com>
2022-09-15 01:13:01 +0530

    router-apis-utils-functions-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import Dashboard from "./container/dashboard/Dashboard";
+import Routers from "./routes/router";
+import { getAllProducts } from "./utils/api/Product";
 
 function App() {
   return (
-    <>
-      <Dashboard />
-    </>
+    <React.Fragment>
+      <Routers />
+    </React.Fragment>
   );
 }
 
