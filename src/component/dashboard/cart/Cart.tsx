2960b4289ac3de1990db1c443951131a10d5a923
Chetan Virani <35021673+viranichetan666@users.noreply.github.com>
2022-09-15 02:14:29 +0530

    product card

--- src/component/dashboard/cart/Cart.tsx
+++ src/component/dashboard/cart/Cart.tsx
@@ -1,31 +1,8 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import { Typography, Button } from "@material-ui/core";
-import testImg from "../../../assets/images/img.jpg";
 
 const Cart = () => {
   return (
     <div>
-      <Card sx={{ maxWidth: 350 }}>
-        <CardMedia component="img" height="200" image={testImg} alt="testImg" />
-        <CardContent>
-          <Typography>Product Name</Typography>
-          <Typography>Description</Typography>
-          <Typography>Quantity</Typography>
-          <Typography>Unit Price</Typography>
-          <Button
-            type="submit"
-            size="medium"
-            color="primary"
-            variant="contained"
-            style={{ marginTop: "10px" }}
-          >
-            Add to cart
-          </Button>
-        </CardContent>
-      </Card>
+      Cart
     </div>
   );
 };
