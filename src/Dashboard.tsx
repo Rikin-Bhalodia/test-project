0b6def84597bd2790e322caef45fd2de1bb5dbde
gunvantpanchal <gunvantpanchal2001@gmail.com>
2022-09-14 21:34:13 +0530

    card component copy paste from material ui

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import Card from "./components/Card";
+
+const jsonData = [
+  {
+    product_name: "hey",
+    product_image: "http://image.com",
+    description: "production  description",
+    quantity: 5,
+    unit_Price: 500,
+  },
+  {
+    product_name: "hey",
+    product_image: "http://image.com",
+    description: "production  description",
+    quantity: 5,
+    unit_Price: 500,
+  },
+];
 
 const Dashboard = () => {
   return (
     <>
-      <div></div>
+      <div className="header">
+        <div> Products</div>
+        <div> Card</div>
+      </div>
+      <div className="product_list">
+        {jsonData.map((cardDetails) => {
+          return <Card props={cardDetails} />;
+        })}
+      </div>
     </>
   );
 };
